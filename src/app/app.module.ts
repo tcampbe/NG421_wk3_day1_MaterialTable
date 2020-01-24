d2a1af9fb7b8c5354a0b4f61f2ba1ca74afd7405
tcampbe <tcampb@icloud.com>
2020-01-23 23:37:50 -0600

    member table ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MembersTableComponent } from './members-table/members-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/members-table/members-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IMember } from "../interfaces/imember";
+import { MemberSService } from "../services/member-s.service";
+import { MatTableModule, MatTableDataSource } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatSort } from "@angular/material/sort";
+
+@Component({
+  selector: "app-members-table",
+  templateUrl: "./members-table.component.html",
+  styleUrls: ["./members-table.component.css"]
+})
+export class MembersTableComponent implements OnInit {
+  displayedColumns: string[] = ["No.", "Name", "Age", "Current"];
+  dataSource: MatTableDataSource<IMember>;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  constructor(private memberSService: MemberSService) {}
+
+  ngOnInit() {
+    this.dataSource = 
+    new MatTableDataSource(this.memberSService.CHARACTER_DATA);
+    this.dataSource.sort = this.sort;
+
+  }
+applyFilter(filterValue: string){
+  this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+}
+}
